71be7696ba3d6b11fac61c0bfee7bc9545aeaa12
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-06-30 16:31:26 -0400

    Split the card into 2 props and tests pass

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,16 +1,30 @@
 # Placing the class within the spec file is NOT best practice but
 # This is the approach taken by this particular tutorial instead of an import
 class Card
-  attr_reader :type
+  attr_reader :value, :suit
 
-  def initialize(type)
-    @type = type
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
   end
 end
 
-RSpec.describe "Card" do
-  it "has a Suit and value as type" do
-    card = Card.new("Ace of Spades")
-    expect(card.type).to eq("Ace of Spades")
+# RSpec.describe "Card" do
+#   it "has a Suit and value as type" do
+#     card = Card.new("Ace of Spades")
+#     expect(card.type).to eq("Ace of Spades")
+#   end
+# end
+# Becomes This. Referencing the class gives helpers to us!
+# Also, splitting to test one aspect of the code is best practice
+RSpec.describe Card do
+  it "has a value" do
+    card = Card.new("Ace", "Spades")
+    expect(card.value).to eq("Ace")
+  end
+
+  it "has a suit" do
+    card = Card.new("Ace", "Spades")
+    expect(card.suit).to eq("Spades")
   end
 end
