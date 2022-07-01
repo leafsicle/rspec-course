4fa0486d67cad1fb6fdc6daaf8f290d4901feaa0
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-07-01 12:52:35 -0400

    updates to repo to use on Personal

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,7 +1,7 @@
 # Placing the class within the spec file is NOT best practice but
 # This is the approach taken by this particular tutorial instead of an import
 class Card
-  attr_reader :value, :suit
+  attr_accessor :value, :suit
 
   def initialize(value, suit)
     @value = value
@@ -15,18 +15,46 @@ end
 #     expect(card.type).to eq("Ace of Spades")
 #   end
 # end
+
 # Becomes This. Referencing the class gives helpers to us!
 # Also, splitting to test one aspect of the code is best practice
+# RSpec.describe Card do
+#   before do
+#     @card = Card.new("Ace", "Spades")
+#   end
+
+#   it "has a value" do
+#     expect(@card.value).to eq("Ace")
+#   end
+
+#   it "has a suit" do
+#     expect(@card.suit).to eq("Spades")
+#   end
+# end
 RSpec.describe Card do
-  before do
-    @card = Card.new("Ace", "Spades")
+  # before do
+  #   @card = Card.new("Ace", "Spades")
+  # end
+  # it "has a value" do
+  #   expect(@card.value).to eq("Ace")
+  # end
+
+  # it "has a suit" do
+  #   expect(@card.suit).to eq("Spades")
+  # end
+  # this can be refactored to provided the same instance of the class
+  # in place of createing a new instanced EACH time an example is run in a group
+  def card
+    Card.new("Ace", "Spades")
   end
 
   it "has a value" do
-    expect(@card.value).to eq("Ace")
+    expect(card.value).to eq("Ace")
+    card.value = "Queen"
+    expect(card.value).to eq("Queen")
   end
 
   it "has a suit" do
-    expect(@card.suit).to eq("Spades")
+    expect(card.suit).to eq("Spades")
   end
 end
