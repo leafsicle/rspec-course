16b03ff8267265efb053ff25e80fca6950946b73
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-07-08 16:55:56 -0400

    YO. Shared examples can use variable names.

--- spec/shared_examples_spec.rb
+++ spec/shared_examples_spec.rb
@@ -1,4 +1,5 @@
-RSpec.shared_examples "a Ruby obj with len == 3" do
+test_name = "a Ruby obj with len == 3"
+RSpec.shared_examples test_name do
   it "returns the number of items" do
     expect(subject.length).to eq(3)
   end
@@ -6,17 +7,17 @@ end
 
 RSpec.describe Array do
   subject { [1, 2, 3] }
-  include_examples "a Ruby obj with len == 3"
+  include_examples test_name
 end
 
 RSpec.describe String do
   subject { "abc" }
-  include_examples "a Ruby obj with len == 3"
+  include_examples test_name
 end
 
 RSpec.describe Hash do
   subject { { a: 1, b: 2, c: 3 } }
-  include_examples "a Ruby obj with len == 3"
+  include_examples test_name
 end
 
 class SausageLink
@@ -27,5 +28,5 @@ end
 
 RSpec.describe SausageLink do
   subject { described_class.new }
-  include_examples "a Ruby obj with len == 3"
+  include_examples test_name
 end
