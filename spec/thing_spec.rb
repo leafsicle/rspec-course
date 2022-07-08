b0277c377d3a7a14b4d2ee2789b1f88828f16390
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-07-08 18:05:47 -0400

    More than 50% done!

---
+++ spec/thing_spec.rb
@@ -0,0 +1,17 @@
+By default, what will subject equal?
+
+How do you define a custom subject? Provide a code example.
+
+How does the described_class helper method work? What is the advantage of using it over writing the class name out?
+
+Write an RSpec test for an array with 2 elements.
+
+Declare the subject to be [1, 2].
+
+Write an example that uses expect(subject) to test whether the array is equal to [1, 2].
+
+Write an example that uses the one-liner is_expected syntax.
+
+Which do you prefer?
+
+What's the difference between include_examples and include_context?
