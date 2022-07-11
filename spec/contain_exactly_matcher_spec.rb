aa23e9020705d3ab9ecc4ddefdf9b753d545c451
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-07-11 14:32:21 -0400

    51/51 contains exactly if contents are all that matter

---
+++ spec/contain_exactly_matcher_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe "contain_exactly matcher" do
+  subject { [1, 2, 3] }
+
+  describe "long form syntax" do
+    it "should check for the presence of all elements" do
+      expect(subject).to contain_exactly(1, 2, 3)
+      expect(subject).to contain_exactly(3, 2, 1)
+      expect(subject).to contain_exactly(2, 1, 3)
+
+      expect(subject).not_to contain_exactly(42, 3, 1)
+    end
+  end
+
+  it { is_expected.to contain_exactly(1, 2, 3) }
+  it { is_expected.to contain_exactly(3, 1, 2) }
+  it { is_expected.to contain_exactly(1, 3, 2) }
+end
