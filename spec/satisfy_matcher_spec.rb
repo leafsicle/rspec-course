2d8e88f3760e1e49ade2712de69d28ce9a9e8725
Matt <cookem529@gmail.com>
2022-07-11 20:50:49 -0400

    Satisfy with custome error messages

--- spec/satisfy_matcher_spec.rb
+++ spec/satisfy_matcher_spec.rb
@@ -1,7 +1,12 @@
 RSpec.describe "satisfies matcher" do
   subject { "racecar" } 
-  it "is a palindrome" do
-    expect(subject).to satisfy { |val| val == 'racecar'}
+    it "is a palindrome" do
+      expect(subject).to satisfy { |val| val == val.reverse }
   end
-  
+  it "can accept a custom error message" do
+    expect(subject).to satisfy("be a palindrome") do |val| 
+      val == val.reverse  
+    end
+  end
+    
 end
