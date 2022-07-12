c7b8c6c3f29fcd46264b26a63b8c4aaf19e1af9e
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-07-11 23:43:41 -0400

    merge conflicts

--- spec/satisfy_matcher_spec.rb
+++ spec/satisfy_matcher_spec.rb
@@ -1,12 +1,11 @@
 RSpec.describe "satisfies matcher" do
-  subject { "racecar" } 
-    it "is a palindrome" do
-      expect(subject).to satisfy { |val| val == val.reverse }
+  subject { "racecar" }
+  it "is a palindrome" do
+    expect(subject).to satisfy { |val| val == val.reverse }
   end
   it "can accept a custom error message" do
-    expect(subject).to satisfy("be a palindrome") do |val| 
-      val == val.reverse  
+    expect(subject).to satisfy("be a palindrome") do |val|
+      val == val.reverse
     end
   end
-    
 end
