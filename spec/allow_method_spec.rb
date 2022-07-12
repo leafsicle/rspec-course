c093427e6b943444d7d187d9272b76a6c7a4110a
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-07-12 14:58:33 -0400

    82 multiple return vals as sequential args

--- spec/allow_method_spec.rb
+++ spec/allow_method_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe "allow method review" do
     arr.push(4)
     expect(arr).to eq([1, 2, 3, 4])
   end
+  it "can return multiple return values in sequence" do
+    mock_array = double
+    allow(mock_array).to receive(:pop).and_return(:c, :b, nil)
+    expect(mock_array.pop).to eq(:c)
+    expect(mock_array.pop).to eq(:b)
+    expect(mock_array.pop).to eq(nil)
+  end
 end
